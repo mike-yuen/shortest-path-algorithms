18fb861600540e69620523425a1da572e7e8747e
Sinh <phamtiensinh010@gmail.com>
2024-05-13 19:04:55 +0700

    fix some bugs in process data from osm files

--- process_osm.py
+++ process_osm.py
@@ -14,7 +14,6 @@ class Node(tp.NamedTuple):
     id: int
     lon: float
     lat: float
-    name: tp.Union[str, None]
 
     # Function to convert latitude and longitude to Cartesian coordinates
     def to_cartesian(self):
@@ -23,11 +22,23 @@ class Node(tp.NamedTuple):
         z = R * math.sin(self.lat)
         return x, y, z
 
+    def __eq__(self, other):
+        return self.id == other.id
+
 
 class Edge(tp.NamedTuple):
     id: str
     nodes: tp.Tuple[Node, Node]
     name: str
+    merged_length: tp.Union[float, None] = None
+
+    def merge_with(self, other):
+        if self == other:
+            return self
+        assert self.nodes[1] == other.nodes[0]
+        return Edge(
+            id=self.id, nodes=(self.nodes[0], other.nodes[1]),
+            name=self.name, merged_length=self.distance() + other.distance())
 
     def distance(self) -> float:
         node1, node2 = self.nodes
@@ -42,21 +53,33 @@ class Edge(tp.NamedTuple):
         d = R * c
         return d
 
+    def __eq__(self, other):
+        return self.id == other.id and self.nodes == other.nodes
+
+
+class EdgeGroup(tp.NamedTuple):
+    edges: tp.List[Edge]
+
+    @staticmethod
+    def merge_edges(edges: tp.List[Edge]):
+        new_edge = edges[0]
+        for edge in edges:
+            new_edge = new_edge.merge_with(edge)
+        return new_edge
+
 
 class OSMProcessHandler(osmium.SimpleHandler):
     def __init__(self):
         super().__init__()
-        self.streets = []
+        self.edge_groups = []
         self.id_to_node = {}
-        self.id_to_node_index = {}
-        self.node_coordinates = []
-        self.pair_coordinates = []
+        self.edge_node_ids = []
 
     def node(self, n):
         # Define your filtering criteria here
         self.id_to_node[n.id] = Node(
             id=n.id, lon=np.radians(n.location.lon),
-            lat=np.radians(n.location.lat), name=n.tags.get('name', ''))
+            lat=np.radians(n.location.lat))
 
     def way(self, way):
         node_refs = way.nodes
@@ -68,51 +91,82 @@ class OSMProcessHandler(osmium.SimpleHandler):
         street_name = way.tags.get('addr:street', None)
         if street_name is None:
             street_name = way.tags.get('name', '')
-        node0 = Node(
-            id=node_ref0.ref, lon=self.id_to_node[node_ref0.ref].lon,
-            lat=self.id_to_node[node_ref0.ref].lat, name=way.tags.get('name', ''))
-        node1 = Node(
-            id=node_ref1.ref, lon=self.id_to_node[node_ref1.ref].lon,
-            lat=self.id_to_node[node_ref1.ref].lat, name=way.tags.get('name', ''))
-        temp = []
-        for node in [node0, node1]:
-            if node.id not in self.id_to_node_index:
-                self.id_to_node_index[node.id] = len(self.id_to_node_index)
-                self.node_coordinates.append(list(node.to_cartesian()[:-1]))
-            temp.append(list(node.to_cartesian()[:-1]))
-        self.pair_coordinates.append(temp)
-        street = Edge(
-            id=way.id, name=street_name, nodes=(node0, node1))
-        self.streets.append(street)
+        self.edge_node_ids.extend([n.ref for n in node_refs])
+        edges = []
+        for i in range(len(node_refs) - 1):
+            nd0 = node_refs[i]
+            nd1 = np.roll(node_refs, -1)[i]
+            node0 = Node(
+                id=nd0.ref, lon=self.id_to_node[nd0.ref].lon,
+                lat=self.id_to_node[nd0.ref].lat)
+            node1 = Node(
+                id=nd1.ref, lon=self.id_to_node[nd1.ref].lon,
+                lat=self.id_to_node[nd1.ref].lat)
+            edge = Edge(id=way.id, name=street_name, nodes=(node0, node1))
+            edges.append(edge)
+        self.edge_groups.append(EdgeGroup(edges=edges))
+
+    def clean(self) -> tp.Tuple[tp.List[Edge], dict]:
+        clean_edges = []
+        id_to_node_index = {}
+        for edge_group in self.edge_groups:
+            edge_merge_queue = []
+            for edge in edge_group.edges:
+                if edge.id == 609435533:
+                    print()
+                edge_nodes =  edge.nodes
+                if self.edge_node_ids.count(edge_nodes[0].id) < 2:
+                    if not len(edge_merge_queue):
+                        # no connection with any edges -> isolated component
+                        continue
+                if self.edge_node_ids.count(edge_nodes[1].id) > 1:
+                    if len(edge_merge_queue):
+                        new_edge = EdgeGroup.merge_edges([*edge_merge_queue, edge])
+                    else:
+                        new_edge = edge
+                    for node in new_edge.nodes:
+                        if node.id not in id_to_node_index:
+                            id_to_node_index[node.id] = len(id_to_node_index)
+                    clean_edges.append(new_edge)
+                    edge_merge_queue.clear()
+                else:
+                    edge_merge_queue.append(edge)
+        return clean_edges, id_to_node_index
 
 
 def get_adjacency_matrix_from_osm_file(filename: str) -> np.ndarray:
     # Parse .osm file and apply filters
     osm_handler = OSMProcessHandler()
     osm_handler.apply_file(filename)
-    node_coordinates = np.array(osm_handler.node_coordinates)
-    plt.figure(figsize=(8, 6))
-    plt.scatter(node_coordinates[:, 0], node_coordinates[:, 1], color='blue', label='Random Data')
-    # Show plot
-    pair_coordinates = np.array(osm_handler.pair_coordinates)
-    for pair in pair_coordinates:
-        plt.plot(
-            pair[:, 0], pair[:, 1],
-            color='red', label='Sine Curve', linestyle='-')
-    plt.grid(True)
-    plt.show()
-    edges = osm_handler.streets
-    print('>>', len(edges))
-    id_to_node_index = osm_handler.id_to_node_index
+    edges, id_to_node_index = osm_handler.clean()
+    # node_coordinates = []
+    # pair_coordinates = []
+    # for node_id in id_to_node_index:
+    #     node_coordinates.append(list(osm_handler.id_to_node[node_id].to_cartesian())[:-1])
+    # node_coordinates = np.array(node_coordinates)
+    # plt.figure(figsize=(8, 6))
+    # plt.scatter(node_coordinates[:, 0], node_coordinates[:, 1], color='blue', label='Random Data')
+    # plt.grid(True)
     num_node = len(id_to_node_index)
     adjacency_matrix = np.zeros((num_node, num_node))
+    print('number of edges', len(edges))
+    print('number of nodes', num_node)
     for edge in edges:
         nodes = edge.nodes
+        # pair_coordinates.append([list(nodes[0].to_cartesian())[:-1], list(nodes[1].to_cartesian())[:-1]])
         node0_id = nodes[0].id
         node1_id = nodes[1].id
         node0_idx = id_to_node_index[node0_id]
         node1_idx = id_to_node_index[node1_id]
-        adjacency_matrix[node0_idx, node1_idx] = edge.distance()
+        adjacency_matrix[node0_idx, node1_idx] = edge.distance() \
+            if edge.merged_length is None else edge.merged_length
+    # print(np.where(adjacency_matrix[148, :] != 0.0))
+    # for idx, pair in enumerate(np.array(pair_coordinates)):
+    #     plt.plot(
+    #         pair[:, 0], pair[:, 1],
+    #         color='red', label='Sine Curve', linestyle='-')
+    # plt.show()
     return adjacency_matrix
 
-print(get_adjacency_matrix_from_osm_file('data/exported_map.osm').shape)
+# a = get_adjacency_matrix_from_osm_file('data/exported_map.osm')
+# print(a.shape)
